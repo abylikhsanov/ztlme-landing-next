e03213e17cad0757ed939964ef3132bf1cef2a3e
Abyl Ikhsanov <abyl@wheel.me>
2023-10-05 22:22:07 +0200

    fixed faq

--- src/components/landing_page/FAQ.tsx
+++ src/components/landing_page/FAQ.tsx
@@ -39,7 +39,7 @@ const textData = [
 
 export const FAQ = () => {
   return (
-    <div className="flex flex-col justify-center items-center max-w-5xl mx-auto mt-12 ml-8 mr-8">
+    <div className="flex flex-col justify-center items-center mt-12 p-4 max-w-5xl mx-auto">
       <div className="flex flex-col w-full max-w-3xl m-4 pb-4">
         <h1 className="font-dm font-bold text-3xl">FAQ</h1>
       </div>
